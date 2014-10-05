1b0c03a97e13f670ebdffba75b6bd2545f707858
George <gschmick@gwu.edu>
2014-10-05 19:36:52 -0400

    Added InventoryController

---
+++ application/modules/admin/controllers/InventoryController.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Inventory Management (required) 
+1) A separate part of the site must contain a interface for administrators to manage the inventory 
+of the site. Each item in the inventory must contain at least the following attributes: 
+ Item Name 
+ Item Description 
+ Price 
+ Date Added 
+ Category 
+2) Administrators must be able to perform the following functions: 
+ Add a new item 
+ Edit an existing item 
+ Delete an existing item 
+ */
+class Admin_InventoryController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        
+    }
+
+}
+
+
+
+
+
+
+

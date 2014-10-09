3fb521e55aa426d99c9d7dddda1f0e044ddc04ea
cluegwu <clue@gwu.edu>
2014-10-09 00:31:05 -0400

    Update and rename Inventory to Inventory.php

--- application/forms/Inventory.php
+++ application/forms/Inventory.php
@@ -1,4 +1,9 @@
- $this->setName('login');
+<?php
+class Application_Form_Login extends Zend_Form
+{
+public function init()
+{
+$this->setName('login');
 $email = new Zend_Form_Element_Text('email');
 $email->setLabel('Email')
 ->setRequired(true)
@@ -15,4 +20,5 @@ $submit = new Zend_Form_Element_Submit('submit');
 $submit->setAttrib('id', 'submitbutton')
 ->setLabel('Login');
 $this->addElements(array($email, $password, $submit));
+}
 }

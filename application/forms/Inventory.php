23a44a71d1ebfb08bb8bf79c6553ca720a05748e
cluegwu <clue@gwu.edu>
2014-10-09 01:05:31 -0400

    Update Inventory.php

--- application/forms/Inventory.php
+++ application/forms/Inventory.php
@@ -1,21 +1,31 @@
 <?php
-class Application_Form_Login extends Zend_Form
+
+class Inventory_Update_Form extends Zend_Form
 {
-public function init()
-{
-$this->setName('login');
-$email = new Zend_Form_Element_Text('email');
-$email->setLabel('Email')
-->setRequired(true)
-->addFilter('StripTags')
-->addFilter('StringTrim')
-->addValidator('NotEmpty');
-$password = new Zend_Form_Element_Password('password');
-$password->setLabel('Password')
-->setRequired(true)
-->addFilter('StripTags')
-->addFilter('StringTrim')
-->addValidator('NotEmpty');
+	public function init()
+	{
+		$this->setName('inventory');
+		
+		$itemName = new Zend_Form_Element_Text('itemName');
+		$itemName->setLabel('Item')
+			->setRequired(true)
+			->addFilter('StripTags')
+			->addFilter('StringTrim')
+			->addValidator('NotEmpty');
+			
+		$itemDescription = new Zend_Form_Element_Password('itemDescription');
+		$itemDescription->setLabel('Item Description')
+			->setRequired(true)
+			->addFilter('StripTags')
+			->addFilter('StringTrim')
+			->addValidator('NotEmpty');
+			
+			$itemName = new Zend_Form_Element_Text('itemName');
+		$itemName->setLabel('Item')
+			->setRequired(true)
+			->addFilter('StripTags')
+			->addFilter('StringTrim')
+			->addValidator('NotEmpty');
 $submit = new Zend_Form_Element_Submit('submit');
 $submit->setAttrib('id', 'submitbutton')
 ->setLabel('Login');

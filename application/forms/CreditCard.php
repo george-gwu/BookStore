13e87d6d42943e9a45713c20ac5f9c7a7c070d58
George <gschmick@gwu.edu>
2014-10-11 16:12:59 -0400

    Custom CC Expiration Validator

--- application/forms/CreditCard.php
+++ application/forms/CreditCard.php
@@ -8,9 +8,11 @@ class Application_Form_CreditCard extends Zend_Form
         
         $cardname = new Zend_Form_Element_Text('cardname');
         $cardname->setLabel('Name on Card')
+              ->setAttrib('size', 40)
               ->setRequired(true)
               ->addFilter('StripTags')
               ->addFilter('StringTrim')
+              ->addFilter('StringToUpper')
               ->addValidator('NotEmpty');
         
         
@@ -28,8 +30,26 @@ class Application_Form_CreditCard extends Zend_Form
               ->setAttrib('size', 7)
               ->addFilter('StripTags')
               ->addFilter('StringTrim')
-              ->addValidator(new Zend_Validate_Date(array('format' => 'MM/YYYY')));
-        
+              ->addValidator(new Zend_Validate_Callback(
+                    function($rawDate){ // Custom Validator (Expires in Future and Before Ten Years) 
+                        // Check Format
+                        $fmtValid = new Zend_Validate_Date(array('format'=>'MM/YYYY'));
+                        if(!$fmtValid->isValid($rawDate)) return false;
+                        // Check if expired
+                        $exp = new Zend_Date($rawDate, 'MM/YYYY');
+                        $exp->addMonth(1)->subSecond(1);
+                        if($exp->isEarlier(Zend_Date::now())){ // if expiration is before current time
+                            return false;
+                        }                
+                        // Check if too far in the future
+                        $tenYears = Zend_Date::now();
+                        $tenYears->addYear(10);
+                        if($exp->isLater($tenYears)){
+                            return false;
+                        }
+                        return true;
+                    })
+                );
         $cardsecurity = new Zend_Form_Element_Text('cardsecurity');
         $cardsecurity->setLabel('Card Security')
               ->setRequired(true)

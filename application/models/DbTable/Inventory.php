0086035181299fc98f68dcfa75f03e3301eb95a9
George <gschmick@gwu.edu>
2014-10-23 17:50:09 -0400

    Bugfixes to Inventory DB Table Schema

--- application/models/DbTable/Inventory.php
+++ application/models/DbTable/Inventory.php
@@ -3,36 +3,43 @@
     class Application_Model_DbTable_Inventory extends Zend_Db_Table_Abstract
     {
         /**
-         * Allows an admin to add a book or article to the inventory.
-         * Q:  Need a key like item Number, is that itemID?   I need this for lookup and updates.
+         * Allows an admin to add a book to the inventory.
+         * Q:  Need a key like item Number, is that itemID?   
+         * A:  it is just ID. since the table is inventory, that is like inventoryID
          */
 
         protected $_name = 'inventory';
 
         public function registerItem($itemName, $itemDescription, $price, $quantity, $categoryID){
             $data = array(
-        /*        'roleType' => App_Acl::ROLEUSER,   May need admin accounts  */
-                'itemName' => $itemName,
-                'itemDescription' => $itemDescription,
-                'price' => $price,
-                'quantity' => $quantity
+                'itemName'          => $itemName,
+                'itemDescription'   => $itemDescription,
+                'price'             => $price,
+                'quantity'          => $quantity,
+                'categoryID'        => $categoryID,
+                'dateCreated'       => Zend_Date::now()->get(Zend_Date::ISO_8601)
             );
             $this->insert($data);
             $itemID = $this->getAdapter()->lastInsertId();
             return $itemID;
         }
+        
         /**
-         * Fix $where ...getItem  
+         * Update Item Inventory Quantity by Reducing by Order Quantity
+         * @param type $ItemID
+         * @param type $orderquantity
          */
-         
         public function updateInventory($ItemID, $orderquantity){
             $data = array(
-                'quantity' => $quantity - $orderquantity)
-            );
-            $where = $this->getItem()->('item = ?', $itemID);
-            $this->update($data, $where);
+                    'quantity' => new Zend_Db_Expr(
+                                $this->getAdapter()->quoteInto('quantity - ?', $orderquantity)
+                            )
+                    );
             
+            $where = $this->getAdapter()->quoteInto('ID = ?', $ItemID);
+            $this->update($data, $where);            
         }
+        
         
         
         /**  
@@ -42,15 +49,29 @@
         */
         
         public function getInvetoryStatus($itemID, $itemName){
-            $select = $this->select()->where ('item <= ?', $itemName)
+            $select = $this->select()->where ('itemName like "%?%"', $itemName);
             $dbResult = $this->fetchRow($select);
             $data = array (
                 'itemName' => $dbResult['itemName'],
                 'itemDescription' => $dbResult['itemDescription'],
-                'price' => $dbResult['price']
+                'price' => $dbResult['price'],
                 'quantity' => $dbResult['quantity']
-                )
+                );
+            
             return $data;
+        }
+        
+        /**
+         * Gets an inventory item by ID
+         * @param type $itemID
+         * @return array
+         */
+        public function getInventoryItemById($itemID){
+            
+            $select = $this->select()->where ('id = ?', $itemID);
+            $dbResult = $this->fetchRow($select);
+            
+            return $dbResult->toArray();                    
         }
         
         
@@ -68,8 +89,5 @@
       PRIMARY KEY (`ID`)
     ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-    */
-
-    public function updateInventory($userID, $cardName, $cardNumber, $expiration, $securityCode){
-    
+    */    
 }
